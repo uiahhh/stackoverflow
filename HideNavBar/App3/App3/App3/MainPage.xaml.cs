1b6662b6e5fbe8dc708f524cd74a2336df89fe37
Ricardo Carvalho <uiahhh@gmail.com>
2018-07-02 10:12:23 -0300

    hidenavbar topic

---
+++ HideNavBar/App3/App3/App3/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using App3.MenuItems;
+using App3.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App3
+{
+    public partial class MainPage : MasterDetailPage
+    {
+        public List<MasterPageItem> menuList { get; set; }
+        public MainPage()
+        {
+            InitializeComponent();
+
+            menuList = new List<MasterPageItem>();
+
+            // Adding menu items to menuList and you can define title ,page and icon
+            menuList.Add(new MasterPageItem() { Title = "Home", Icon = "home.png", TargetType = typeof(HomePage) });
+            menuList.Add(new MasterPageItem() { Title = "Settings", Icon = "setting.png", TargetType = typeof(SettingsPage) });
+            menuList.Add(new MasterPageItem() { Title = "Help", Icon = "help.png", TargetType = typeof(HelpPage) });
+
+            // Setting our list to be ItemSource for ListView in MainPage.xaml
+            navigationDrawerList.ItemsSource = menuList;
+
+            // Initial navigation, this can be used for our home page
+            var navPage = new NavigationPage((Page)Activator.CreateInstance(typeof(HomePage)));
+
+            NavigationPage.SetHasNavigationBar(navPage, false);
+            NavigationPage.SetHasBackButton(navPage, false);
+
+            Detail = navPage;
+        }
+
+        // Event for Menu Item selection, here we are going to handle navigation based
+        // on user selection in menu ListView
+        private void OnMenuItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+
+            var item = (MasterPageItem)e.SelectedItem;
+            Type page = item.TargetType;
+
+            Detail = new NavigationPage((Page)Activator.CreateInstance(page));
+            IsPresented = false;
+        }
+    }
+}
---
+++ HideNavBar/App3/App3/App3/MenuItems/MasterPageItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App3.MenuItems
+{
+    public class MasterPageItem
+    {
+        public string Title { get; set; }
+
+        public string Icon { get; set; }
+
+        public Type TargetType { get; set; }
+    }
+}
